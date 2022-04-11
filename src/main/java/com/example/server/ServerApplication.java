3bd53c26208ec87d86488a5987f794c3e1dfc8f8
Faruk Shaban <faruk.saban@live.com>
2022-04-11 21:51:32 +0200

    update

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -7,20 +7,44 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 @SpringBootApplication
 public class ServerApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(ServerApplication.class, args);
 	}
 	@Bean
-	CommandLineRunner run(ServerRepo serverRepo){
+	CommandLineRunner run (ServerRepo serverRepo){
 		return args -> {
 			serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 gb", "Personal PC",
 					"http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.58", "Windows", "32 gb", "Work PC",
+					"http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.107", "Distro", "128 gb", "Just PC",
+					"http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.25", "Mobile", "64 gb", "Mutual PC",
+					"http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
 		};
 	}
+    @Bean
+    public org.springframework.web.filter.CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new org.springframework.web.filter.CorsFilter(urlBasedCorsConfigurationSource);
+    }
 
 }
